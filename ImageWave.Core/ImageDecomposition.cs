860d5057e01428a73f455fd9e950ae6ecea4226f
Trevor D'Arcy-Evans <trevor.darcyevans@apdcomms.com>
2022-04-08 11:41:26 +0100

    - WIP initial commit of ImageWave

---
+++ ImageWave.Core/ImageDecomposition.cs
@@ -0,0 +1,39 @@
+﻿using WaveletStudio;
+
+namespace ImageWave.Core;
+
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.PixelFormats;
+using SixLabors.ImageSharp.Processing;
+using WaveletStudio.Wavelet;
+
+public sealed class ImageDecomposition
+{
+  public static List<DecompositionLevel> Decompose(Image<Rgba32> img, int hashSideSize = 32)
+  {
+    // resize img to 16x16px (by default) or with configured size 
+    img.Mutate(ctx => ctx
+      .Resize(new Size(hashSideSize, hashSideSize))
+      .BlackWhite());
+
+    var input = new List<double>();
+    img.ProcessPixelRows(acc =>
+    {
+      for (var y = 0; y < acc.Height; y++)
+      {
+        var pxRow = acc.GetRowSpan(y);
+        for (var x = 0; x < pxRow.Length - 1; x++)
+        {
+          ref var px = ref pxRow[x];
+          var isWhite = (px.R + px.G + px.B) > 45;
+          input.Add(isWhite ? 1d : 0d);
+        }
+      }
+    });
+    var signal = new Signal(input.ToArray());
+    var wavelet = MotherWavelet.LoadFromName("haar");
+    var decomp = DWT.ExecuteDWT(signal, wavelet, 1);
+
+    return decomp;
+  }
+}
---
+++ ImageWave.UI.CLI/Program.cs
@@ -0,0 +1,48 @@
+﻿using ImageWave.Core;
+
+namespace ImageWave.UI.CLI;
+
+using CommandLine;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.PixelFormats;
+
+internal static class Program
+{
+  public static async Task Main(string[] args)
+  {
+    var result = await Parser.Default.ParseArguments<Options>(args)
+      .WithParsedAsync(Run);
+    await result.WithNotParsedAsync(HandleParseError);
+  }
+
+  private static async Task Run(Options opt)
+  {
+    var img1 = await Image.LoadAsync<Rgba32>(opt.ImageFile1Path);
+    var img2 = await Image.LoadAsync<Rgba32>(opt.ImageFile2Path);
+    var imgDecomp1 = ImageDecomposition.Decompose(img1);
+    var imgDecomp2 = ImageDecomposition.Decompose(img2);
+    var match = 90d;
+    Console.WriteLine($"ImageFile1 = {opt.ImageFile1Path}");
+    Console.WriteLine($"ImageFile2 = {opt.ImageFile2Path}");
+    Console.WriteLine($"  Match = {match}%");
+  }
+
+  private static Task HandleParseError(IEnumerable<Error> errs)
+  {
+    if (errs.IsVersion())
+    {
+      Console.WriteLine("Version Request");
+      return Task.CompletedTask;
+    }
+
+    if (errs.IsHelp())
+    {
+      Console.WriteLine("Help Request");
+      return Task.CompletedTask;
+      ;
+    }
+
+    Console.WriteLine("Parser Fail");
+    return Task.CompletedTask;
+  }
+}
